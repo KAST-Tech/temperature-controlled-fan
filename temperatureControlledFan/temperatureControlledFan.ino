3ea41950f3641cf1f2209ddb679bbca964f016e9
Monmoy042 <monmoykms.16@gmail.com>
2021-07-13 22:31:06 +0600

    Initializing the project

---
+++ temperatureControlledFan/temperatureControlledFan.ino
@@ -0,0 +1,85 @@
+int temp = A0;
+int greenLed = 2;
+int redLed = 4;
+int fan = 7;
+int buzzer = 8;
+
+int thresholdValue = 0;
+int celsius = 0;
+int fahrenheit = 0;
+
+void greenLightOn(){
+  digitalWrite(greenLed, HIGH);
+}
+void greenLightOff(){
+  digitalWrite(greenLed, LOW);
+}
+void redLightOn(){
+  digitalWrite(redLed, HIGH);
+}
+void redLightOff(){
+  digitalWrite(redLed, LOW);
+}
+void fanOn(){
+  digitalWrite(fan, HIGH);
+}
+void fanOff(){
+  digitalWrite(fan, LOW);
+}
+void buzzerOn(){
+  digitalWrite(buzzer, HIGH);
+}
+void buzzerOff(){
+  digitalWrite(buzzer, LOW);
+}
+
+void setup()
+{
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(temp, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  //thresholdValue = 38;
+  
+  celsius = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+  fahrenheit = ((celsius * 9) / 5 + 32);
+  
+  Serial.print(celsius);
+  Serial.print(" C : ");
+  Serial.print(fahrenheit);
+  Serial.println(" F");
+  
+  if( celsius<= 30){
+    //digitalWrite(greenLed,HIGH);
+    greenLightOn();
+    redLightOff();
+    fanOff();
+    buzzerOff;
+    //Serial.println("green light on");
+    
+  }
+  else if(celsius >= 31 && celsius <= 40){
+    digitalWrite(greenLed, LOW);
+    digitalWrite(fan, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(redLed, HIGH);
+    //Serial.println("red light on");
+  }
+  else if(celsius > 40){
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(fan, HIGH);
+    digitalWrite(buzzer, HIGH);
+    //Serial.println("Red Light On | Fan on");
+  }
+  else{
+    Serial.println("Temperature is Normal");
+  }
+  delay(1000);
+}
